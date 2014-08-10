1ce204aae125c72366a56cb3eee73f3d8a93b11b
neuromanxr <kelv17@gmail.com>
2014-08-10 16:56:56 -0400

    Initial commit from local git

---
+++ BNR-iOS-Nerdfeed/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  BNR-iOS-Nerdfeed
+//
+//  Created by Kelvin Lee on 8/10/14.
+//  Copyright (c) 2014 Kelvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BNR-iOS-Nerdfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BNR-iOS-Nerdfeed
+//
+//  Created by Kelvin Lee on 8/10/14.
+//  Copyright (c) 2014 Kelvin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
